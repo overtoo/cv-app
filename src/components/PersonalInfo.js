5bdaeb39844d244bb893b641929331cff6082f4c
overtoo <s_bajo@hotmail.com>
2021-08-05 14:46:19 +0800

    second commit

---
+++ src/components/PersonalInfo.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const PersonalInfo = (props) => {
+  return (
+    <form>
+      <p>Personal Details</p>
+      <input
+        onChange={this.handleChange}
+        value={task.text}
+        type="text"
+        id="name"
+      />
+      <input
+        onChange={this.handleChange}
+        value={task.text}
+        type="text"
+        id="name"
+      />
+      <input
+        onChange={this.handleChange}
+        value={task.text}
+        type="text"
+        id="name"
+      />
+    </form>
+  );
+};
